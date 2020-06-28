fa6598e467386d98f8e95c88e764c2d14480092d
Salekin Imran <abir045@gmail.com>
2020-06-28 19:45:51 +0600

    Updates

---
+++ precache-manifest.dcd5b2855e58b3555d5da151fea5e716.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57d620fb7c70a4debb69d20922097925",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ada006d2e31b93c464f",
+    "url": "/static/css/main.b74c4074.chunk.css"
+  },
+  {
+    "revision": "41f661d77ae4363519d3",
+    "url": "/static/js/2.f37af342.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f37af342.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ada006d2e31b93c464f",
+    "url": "/static/js/main.f890d506.chunk.js"
+  },
+  {
+    "revision": "113609605c66f6828f80",
+    "url": "/static/js/runtime-main.cadb1c46.js"
+  }
+]);
